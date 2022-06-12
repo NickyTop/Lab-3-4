cb39f8a68d157b48cc163de5ca0410d1cf1e2b60
NickyTop <nick.soroka91@gmail.com>
2022-06-12 22:51:08 +0300

    Lab 3-4 done

---
+++ Лаб 4 (SQL)/ConnectToSQLServer/ConnectToSQLServer/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System.Windows;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System;
+using System.Windows.Controls;
+
+namespace ConnectToSQLServer
+{
+    public partial class MainWindow : Window
+    {
+        string connectionString = null;        
+        SqlConnection connection = null;
+        SqlCommand command;
+        SqlDataAdapter adapter;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            //MessageBox.Show(connectionString);
+            GetPerformersData();
+            //GetMusicansData();
+            //GetTradeData();
+            GetRecordsData();
+        }
+
+        private void GetAndDhowData(string SQLQuery, DataGrid dataGrid)
+        {
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            command = new SqlCommand(SQLQuery, connection);
+            adapter = new SqlDataAdapter(command);
+            DataTable Table = new DataTable();
+            adapter.Fill(Table);
+            dataGrid.ItemsSource = Table.DefaultView;
+            connection.Close();            
+        }
+
+        private void GetPerformersData()
+        {
+            string sqlQ = "SELECT TOP(20) Name as [Назва], ThisYearSales as [Продажів за рік]" +
+                " From Records R FULL JOIN Trade T " +
+                "ON R.IDRecords = T.IDRecords ORDER BY ThisYearSales DESC";
+            try
+            {
+                GetAndDhowData(sqlQ, PerformersDG);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        /*private void GetMusicansData()
+        {
+            string sqlQ = "";
+            try
+            {
+                GetAndDhowData(sqlQ, MusicansDG);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }*/
+
+       /*private void GetTradeData()
+        {
+            string sqlQ = "";
+            try
+            {
+                GetAndDhowData(sqlQ, TradeDG);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }*/
+
+        private void GetRecordsData()
+        {
+            string sqlQ = "SELECT Performer as [Виконавець], Name as [Назва Альбому] From Records WHERE Performer = 'Nirvana'; ";
+            try
+            {
+                GetAndDhowData(sqlQ, RecordsDG);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+}
